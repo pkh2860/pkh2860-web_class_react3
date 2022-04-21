3fdf61ffb8004ceb5616abf3468557880a2ca4cf
pkh2860 <97070047+pkh2860@users.noreply.github.com>
2022-04-21 09:33:18 +0900

    2022.04.21.09

--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -1,123 +1,103 @@
-// import React from "react";
-// import Header from "../layout/Header";
-// import Contents from "../layout/Contents";
-// import Title from "../layout/Title";
-// import ReferCont from "../includes/ReferCont";
-// import Footer from "../layout/Footer";
-
-// function Reference(){
-//     return (
-//         <>
-//             <Header />
-//             <Contents>
-//                 <Title title={["HTML", "REFERENCE"]} />
-//                 <ReferCont />
-//                 <ContContact />
-//             </Contents>
-//             <Footer />
-//         </>
-//     )
-// }
-
-// export default Reference;
-
 import React from "react";
 import Header from "../layout/Header";
 import Contents from "../layout/Contents";
-import Title from "../layout/Title";
-import ReferCont from "../includes/ReferCont";
 import Footer from "../layout/Footer";
-import ContContact from '../layout/Contact';
-import Loading from "../basics/Loading"
+import Title from "../layout/Title";
+import ContContact  from "../layout/Contact";
+import ReferCont from "../includes/ReferCont";
+import Loading from "../basics/Loading";
 import { gsap } from "gsap";
+import axios from "axios";
 // function Reference(){
 //     return (
 //         <>
-//             <Header color="light"/>
+//             <Header color="light" />
 //             <Contents>
-//                 <Title title={[ "REFERENCE", "book"]} color="light" />
+//                 <Title title={["reference","book"]} color="light" />
 //                 <ReferCont color="light" />
-//                 <ContContact />
+//                 <Contact />
 //             </Contents>
-
-//             <Footer  color="light"/>
+//             <Footer color="light" />
 //         </>
 //     )
 // }
-
 class Reference extends React.Component {
     state = {
         isLoading: true,
+        refers: [],
     }
-
-    ReferAnimation = () => {
-        setTimeout(()=>{
+    mainAnimation = () => {
+        setTimeout(() => {
             gsap.to("#header", {
-                duration:1.0,
+                duration: 0.8,
                 top: 0,
             });
             gsap.to("#footer", {
-                duration:0.8,
+                duration: 0.8,
                 bottom: 0,
-                // delay: 0.2,
+                delay: 0.2,
             });
             gsap.to(".cont__title strong", {
-                duration: 0.5,
-                x:0,
-                y:0,
-                opacity:1,
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
                 delay: 1.0,
-                ease: "circ.out"
-            })
+                ease: "power4.out"
+            });
             gsap.to(".cont__title em", {
-                duration: 0.5,
-                x:0,
-                y:0,
-                opacity:1,
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
                 delay: 1.3,
-                ease: "circ.out"
-            })
+                ease: "power4.out"
+            });
             gsap.to(".refer__inner", {
-                duration: 1.0,
-                // x:0,
-                y:0,
-                opacity:1,
+                duration: 0.7,
+                y: 0,
+                opacity: 1,
                 delay: 1.5,
-                ease: "circ.out"
-            })
-        }, 1000)
+                ease: "power4.out"
+            });
+        }, 10)
+    }
+    getSite = async () => {
+        const {
+                data: {
+                    data : {htmlRefer},
+            },
+        } = await axios.get("https://webstoryboy.github.io/react2022/src/assets/json/refer.json");
+        this.setState({refers: htmlRefer, isLoading: false})
+        this.mainAnimation();
     }
 
-
-    componentDidMount(){    //3초뒤에 로딩이 끝난 후 메인 화면이 나옴
+    componentDidMount(){
         setTimeout(() => {
-            document.getElementById("loading").classList.remove("loading__active")
-            this.setState({isLoading:false})    //true 실행 3초 후 false로 변경
-            this.ReferAnimation()
-        }, 3000)
+            document.getElementById("loading").classList.remove("loading__active");
+            document.querySelector("body").style.background = "#F0EEEB";
+            this.getSite();
+        }, 2000);
     }
-
     render(){
-        const {isLoading} = this.state  //this.state = true
+        const {isLoading, refers} = this.state;
 
-    return (
-        <>          
-             {isLoading ? ( //true = Loading 실행 false = header~ 실행
-            <Loading color="light" />
-            ) : (
-                <>
-                    <Header color="light"/>
-                    <Contents>
-                        <Title title={[ "REFERENCE", "book"]} color="light" />
-                        <ReferCont color="light" />
-                        <ContContact />
-                    </Contents>
-                    <Footer  color="light"/>
-                </>
-            )}
-        </>  
-    )
-}
-}
 
+        return (
+            <>
+                {isLoading ? (
+                    <Loading color="light" />
+                    ):(
+                    <>
+                        <Header color="light" />
+                        <Contents>
+                            <Title title={["Reference","book"]} color="light" />
+                            <ReferCont refer={refers} color="light" />
+                            <ContContact />
+                        </Contents>
+                        <Footer color="light" />
+                    </>
+                )}
+            </>
+        )
+    }
+}
 export default Reference;
