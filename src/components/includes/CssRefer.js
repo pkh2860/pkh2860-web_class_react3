cbbd9f9b05c9f3c0cef66659bd983c93519e297e
pkh2860 <97070047+pkh2860@users.noreply.github.com>
2022-06-09 15:41:36 +0900

    2022.06.09

--- src/components/includes/CssRefer.js
+++ src/components/includes/CssRefer.js
@@ -1,19 +1,12 @@
 import React from "react";
-import ReferCont from "./ReferCont";
 import axios from "axios";
+import ReferCont from "../includes/ReferCont";
 
 class CssRefer extends React.Component {
   state = {
     cssRefer: [],
   };
-    constructor(props) {
-      super();
-  
-      this.state = {
-        menu: 1,
-      };
-    }
-    
+
   getRefer = async () => {
     const {
       data: {
@@ -21,45 +14,44 @@ class CssRefer extends React.Component {
       },
     } = await axios.get(
       "https://pkh2860.github.io/pkh2860-web_class_react3/src/assets/json/referenceCss.json"
-
     );
 
     this.setState({ cssRefer });
   };
 
+
   componentDidMount() {
     this.getRefer();
   }
 
+  render() {
+    const { cssRefer } = this.state;
 
-    render(){
-      const { CssRefer } = this.state;
-        return(
-          <>
-          {CssRefer.map((refer) => (
-            <ReferCont
-              key={refer.id}
-              id={refer.id}
-              title={refer.title}
-              desc={refer.desc}
-              use={refer.use}
-              desc2={refer.desc2}
-              element={refer.element}
-              tag={refer.tag}
-              version={refer.version}
-              view={refer.view}
-              image={refer.image}
-              link={refer.link}
-              Definition={refer.Definition}
-              Accessibility={refer.Accessibility}
-              mdn={refer.mdn}
-              w3c={refer.w3c}
-            />
-          ))}
-        </>
-        )
-    }
+    return (
+      <>
+        {cssRefer.map((refer) => (
+          <ReferCont
+            key={refer.id}
+            id={refer.id}
+            title={refer.title}
+            desc={refer.desc}
+            use={refer.use}
+            desc2={refer.desc2}
+            element={refer.element}
+            tag={refer.tag}
+            version={refer.version}
+            view={refer.view}
+            image={refer.image}
+            link={refer.link}
+            Definition={refer.Definition}
+            Accessibility={refer.Accessibility}
+            mdn={refer.mdn}
+            w3c={refer.w3c}
+          />
+        ))}
+      </>
+    );
   }
-  
-  
-  export default CssRefer;
+}
+
+export default CssRefer;
